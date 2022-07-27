5b980128fb67790ef399a772b1496d3302bb5b92
Francesco Albanese <falbanese96@gmail.com>
2022-07-26 22:39:36 -0400

    User model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address',
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual(
+  'friendCount'.get(function () {
+    return this.friends.length;
+  })
+);
+
+const User = model('User', studentSchema);
+
+module.exports = User;
